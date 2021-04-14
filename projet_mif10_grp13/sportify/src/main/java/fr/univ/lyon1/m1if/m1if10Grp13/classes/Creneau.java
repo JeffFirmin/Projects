dc33781292c525d6e1cd1f686292561f1e78d082
JeffFirmin <75492770+JeffFirmin@users.noreply.github.com>
2021-04-14 18:14:16 +0200

    sportify

---
+++ projet_mif10_grp13/sportify/src/main/java/fr/univ/lyon1/m1if/m1if10Grp13/classes/Creneau.java
@@ -0,0 +1,56 @@
+package fr.univ.lyon1.m1if.m1if10Grp13.classes;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "creneau")
+@IdClass(CreneauCompositeKey.class)
+public class Creneau {
+    @Id
+    private Date datecreneau;
+    @Id
+    private Date heurecreneau;
+    private Date duree;
+
+    public Creneau(Date dateCreneau, Date heureCreneau, Date duree) {
+        this.datecreneau = dateCreneau;
+        this.heurecreneau = heureCreneau;
+        this.duree = duree;
+    }
+
+    public Creneau() {
+
+    }
+
+    public Date getDateCreneau() {
+        return datecreneau;
+    }
+
+    public void setDateCreneau(Date dateCreneau) {
+        this.datecreneau = dateCreneau;
+    }
+
+    public Date getHeureCreneau() {
+        return heurecreneau;
+    }
+
+    public void setHeureCreneau(Date heureCreneau) {
+        this.heurecreneau = heureCreneau;
+    }
+
+    public Date getDuree() {
+        return duree;
+    }
+
+    public void setDuree(Date duree) {
+        this.duree = duree;
+    }
+
+}
